c7d3515a3a4c247b9100e9c361e89a1b644fa062
Ghazi <riskyghazi@gmail.com>
2024-10-23 20:06:03 +0400

    Update page title, description, and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,22 +21,18 @@ const tajawal = Tajawal({
 });
 
 export const metadata: Metadata = {
-  title: "Your New Landing Page",
-  description: "A modern and innovative landing page for your project",
+  title: 'Alphadhad x - AX',
+  description: 'Be part of Alphadhad x community - Be BOLD!',
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="ar" dir="rtl" className={`${tajawal.variable} rtl`}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
-  );
+  )
 }
